dd9605d8b83181ecbeed98b3ed686a4d28488bd1
Amol Ghotale <ghotaleamol@gmail.com>
2022-05-26 14:51:16 +0530

    ADDED: Lombal library for logging

--- src/main/java/com/bridgelabz/addressbookspringboot/AddressBookSpringBootApplication.java
+++ src/main/java/com/bridgelabz/addressbookspringboot/AddressBookSpringBootApplication.java
@@ -1,13 +1,16 @@
 package com.bridgelabz.addressbookspringboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookSpringBootApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(AddressBookSpringBootApplication.class, args);
+    log.info("Address Book Application Started!!!");
     System.out.println("Welcome to Address Book Program");
   }
 }
